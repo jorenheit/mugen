96c988be138bbb0188062b2e088cb3e28ec6f316
Joren Heit <jorenheit@gmail.com>
2025-03-30 12:09:25 +0200

    more extensive help message

--- src/main.cc
+++ src/main.cc
@@ -4,16 +4,29 @@
 #include "parser.h"
 
 void printHelp(std::string const &progName) {
-    std::cout << "Usage: " << progName << " <specification-file> <output-file>\n";
+    std::cout << "Usage: " << progName << " <specification-file (.mu)> <output-file>\n\n";
+    std::cout << "Mugen is a microcode generator that converts a specification file\n"
+              << "into microcode images suitable for flashing onto ROM chips.\n"
+	      << "See https://github.com/jorenheit/mugen/tree/main for more help.\n\n"
+              << "Options:\n"
+              << "  -h, --help    Display this help message and exit\n\n"
+              << "Example:\n"
+              << "  " << progName << " myspec.mu microcode.bin\n";
 }
 
 int main(int argc, char **argv) {
-        
-    if (argc != 3) {
-	printHelp(argv[0]);
-	return 1;
+
+    if (argc == 2 && (std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help")) {
+        printHelp(argv[0]);
+        return 0;
     }
-        
+
+    if (argc != 3) {
+        std::cerr << "Error: Invalid number of arguments.\n\n";
+        printHelp(argv[0]);
+        return 1;
+    }
+
     std::string inFilename = argv[1];
     std::string outFilename = argv[2];
         
