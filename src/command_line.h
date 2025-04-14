3efd468636b246fadd6fa5b43c59fa7b43b53d6c
Joren Heit <jorenheit@gmail.com>
2025-04-14 21:52:49 +0200

    untabify

--- src/command_line.h
+++ src/command_line.h
@@ -10,32 +10,32 @@ private:
 public:
     template <typename Callable>
     void add(std::string const &cmdName, Callable&& fun) {
-	assert(!cmdMap.contains(cmdName) && "Duplicate command");
+        assert(!cmdMap.contains(cmdName) && "Duplicate command");
     
-	cmdMap.insert({cmdName, [=](CommandArgs const &args) -> CommandReturn {
-	    if constexpr (std::is_same_v<decltype(fun(args)), void>) {
-		fun(args);
-		return {false, false};
-	    }
-	    else {
-		return fun(args);
-	    }
-	}});
+        cmdMap.insert({cmdName, [=](CommandArgs const &args) -> CommandReturn {
+            if constexpr (std::is_same_v<decltype(fun(args)), void>) {
+                fun(args);
+                return {false, false};
+            }
+            else {
+                return fun(args);
+            }
+        }});
     }
   
     template <typename Callable>
     void add(std::initializer_list<std::string> const &aliases, Callable &&fun) {
-	for (std::string const &cmdName: aliases) {
-	    add(cmdName, std::forward<Callable>(fun));
-	}
+        for (std::string const &cmdName: aliases) {
+            add(cmdName, std::forward<Callable>(fun));
+        }
     }
   
     CommandReturn exec(std::vector<std::string> const &args) {
-	assert(args.size() > 0 && "empty arg list");
-	if (!cmdMap.contains(args[0])) {
-	    debug_error(args[0], "Unknown command.");
-	    return {false, false};
-	}
-	return (cmdMap.find(args[0])->second)(args);
+        assert(args.size() > 0 && "empty arg list");
+        if (!cmdMap.contains(args[0])) {
+            debug_error(args[0], "Unknown command.");
+            return {false, false};
+        }
+        return (cmdMap.find(args[0])->second)(args);
     }
 };
--- src/mugen.h
+++ src/mugen.h
@@ -7,7 +7,7 @@
 namespace Mugen {
 
     struct RomSpecs {
-	size_t rom_count;
+        size_t rom_count;
         size_t word_count;
         size_t bits_per_word;
         size_t address_bits;
@@ -23,11 +23,11 @@ namespace Mugen {
         size_t flag_bits = 0;
         size_t flag_bits_start = 0;
 
-	size_t segment_bits = 0;
-	size_t segment_bits_start = 0;
+        size_t segment_bits = 0;
+        size_t segment_bits_start = 0;
 
-	size_t total_address_bits = 0;
-	std::vector<std::string> flag_labels;
+        size_t total_address_bits = 0;
+        std::vector<std::string> flag_labels;
     };
 
     using Opcodes = std::unordered_map<std::string, size_t>;
@@ -35,30 +35,30 @@ namespace Mugen {
     using Image = std::vector<unsigned char>;
 
     struct Options {
-	enum class Padding {
-	    NONE,
-	    VALUE,
-	    CATCH
-	};
-	
-	bool printLayout = false;
-	bool lsbFirst = true;
-	Padding padImages = Padding::NONE;
-	unsigned char padValue = 0;
+        enum class Padding {
+            NONE,
+            VALUE,
+            CATCH
+        };
+        
+        bool printLayout = false;
+        bool lsbFirst = true;
+        Padding padImages = Padding::NONE;
+        unsigned char padValue = 0;
     };
     
     struct Result {
-	std::vector<Image> images;
-	std::string layout;
+        std::vector<Image> images;
+        std::string layout;
 
-	Opcodes opcodes;
-	AddressMapping address;
-	Signals signals;
-	RomSpecs rom;
+        Opcodes opcodes;
+        AddressMapping address;
+        Signals signals;
+        RomSpecs rom;
     };
 
     Result generate(std::string const &specFile, Options const &opt);
-    bool debug(std::string const &progName, std::string const &specFile, std::string const &outFileBase, Result const &result);
+    bool debug(std::string const &specFile, std::string const &outFileBase, Result const &result);
 }
 
 #endif
